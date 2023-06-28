6a13e8e4b09220357aa67d8995593c9cf79724ca
void-main1812 <sour.bt21cs56@opju.ac.in>
2023-06-28 14:35:09 +0530

    ✨🖼️ Added: Image hover animation

--- app/(site)/components/Body.tsx
+++ app/(site)/components/Body.tsx
@@ -5,6 +5,7 @@ import { Play } from "next/font/google";
 import { Rowdies } from "next/font/google";
 import Button from "@/app/components/Button";
 import Image from "next/image";
+import { useSpring, a } from "react-spring";
 
 const play = Play({
   subsets: ["latin"],
@@ -17,6 +18,29 @@ const rowdies = Rowdies({
 });
 
 const Body = () => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 10, tension: 500, friction: 40 },
+  }));
+
+  const calc = (x: number, y: number) => [
+    (y - window.innerHeight / 2) / 50,
+    (x - window.innerWidth / 2) / 50,
+    1,
+  ];
+
+  const handleMouseMove = (x: number, y: number) => {
+    const data = calc(x, y);
+    set({ xys: data });
+  };
+
+  const handleMouseLeave = () => {
+    set({ xys: [0, 0, 1] });
+  };
+
+  const trans = (x: number, y: number, s: number) =>
+    `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
   return (
     <div className=" mt-[25vh] z-0 flex flex-col items-center ">
       <div className="h-[60vh] w-[80vw] bg-purple-800 blur-[50vh] rounded-full absolute top-[-30vh]" />
@@ -43,13 +67,24 @@ const Body = () => {
           JOIN NOW
         </Button>
       </div>
-      <Image
-        src={"/images/code.png"}
-        alt="coding preview"
-        height={619}
-        width={991}
-        className="mt-16 border-8 border-gray-50/20 rounded-3xl "
-      />
+      <a.div
+        className="group relative"
+        onMouseMove={({ clientX, clientY }) =>
+          handleMouseMove(clientX, clientY)
+        }
+        onMouseLeave={handleMouseLeave}
+        style={{ transform: props.xys.to(trans) }}
+      >
+        <div className="h-[80vh] opacity-0 transition-opacity duration-500 group-hover:opacity-100 w-[40vh] bg-purple-600 blur-[30vh] rounded-full absolute -left-8 top-10" />
+        <div className="h-[80vh] opacity-0 transition-opacity duration-500 group-hover:opacity-100 w-[60vh] bg-indigo-700 blur-[40vh] rounded-full -right-8 absolute -bottom-10" />
+        <Image
+          src={"/images/code.png"}
+          alt="coding preview"
+          height={619}
+          width={991}
+          className="mt-16 border-[1px] relative border-purple-900 rounded-xl "
+        />
+      </a.div>
     </div>
   );
 };
