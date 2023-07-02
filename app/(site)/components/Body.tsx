ab4c5944a9e4f00647f19bc7cddbb478af504e19
void-main1812 <sour.bt21cs56@opju.ac.in>
2023-07-02 21:31:32 +0530

    🎉📚 Completed: Landing Page UI

--- app/(site)/components/Body.tsx
+++ app/(site)/components/Body.tsx
@@ -12,6 +12,7 @@ import {Player} from "@lottiefiles/react-lottie-player";
 import UserItem from "@/app/components/UserItem";
 import MemberDetails from "@/app/components/MemberDetails";
 import EventsCard from "@/app/components/EventsCard";
+import VolunteerOpening from "@/app/(site)/components/VolunteerOpening";
 
 const play = Play({
     subsets: ["latin"],
@@ -20,7 +21,6 @@ const play = Play({
 
 const inter = Inter({
     subsets: ["latin"],
-    weight: "400",
 });
 
 const rowdies = Rowdies({
@@ -243,7 +243,7 @@ const Body = () => {
 
             {/* --------------------------------------Advantages of Decode------------------------------------- */}
 
-            <div className='container flex flex-col justify-center items-center'>
+            <div className='container flex flex-col justify-center items-center' id='about' >
                 <p className={`text-6xl text-transparent font-light bg-clip-text bg-gradient-to-b from-gray-100/80 to-gray-600/80 tracking-wider text-center leading-[12vh] ${rowdies.className}`}>WHY
                     SHOULD YOU<br/>JOIN DECODE</p>
                 <p className={`w-[60vw] text-center text-gray-400 mt-4 font-extralight ${inter.className}`}>Because here
@@ -298,10 +298,11 @@ const Body = () => {
 
             {/* ------------------------------------Core Team Section------------------------------------------ */}
 
-            <div className='container flex flex-col gap-8 justify-center items-center mt-20 mb-10 '>
+            <div className='container flex flex-col gap-8 justify-center items-center mt-20 mb-10 ' id='members' >
                 <h1 className={`text-6xl text-transparent bg-clip-text bg-gradient-to-b from-gray-100/80 to-gray-600/80 uppercase font-bold ${rowdies.className} `}>Core
                     Members</h1>
-                <p className={`text-gray-500 font-extralight text-lg w-[50vw] text-center ${inter.className} `}>These efforts
+                <p className={`text-gray-500 font-extralight text-lg w-[50vw] text-center ${inter.className} `}>These
+                    efforts
                     can&apos;t be successful without the help of our core team members so let&apos;s
                     appreciate their efforts by supporting them on various platforms. </p>
                 <div className='flex relative justify-center w-[95vw] items-center'>
@@ -326,11 +327,15 @@ const Body = () => {
                 </div>
             </div>
 
+            {/*  -----------------------------------------Registration for Volunteers----------------------------------  */}
+            <VolunteerOpening/>
+
             {/* --------------------------------------------Events & Activities-----------------------------------  */}
-            <div className='container flex flex-col gap-8 justify-center items-center relative mt-10 mb-10'>
+            <div className='container flex flex-col gap-8 justify-center items-center relative mt-10 mb-10' id='events' >
                 <h1 className={`text-6xl text-transparent bg-clip-text bg-gradient-to-b from-gray-100/80 to-gray-600/80 uppercase font-bold ${rowdies.className} `}>Events
                     & Activities</h1>
-                <p className={`text-gray-500 font-extralight text-lg w-[50vw] text-center ${inter.className} `}>There will be
+                <p className={`text-gray-500 font-extralight text-lg w-[50vw] text-center ${inter.className} `}>There
+                    will be
                     various Events and activities under the Community which will enhance your skills and motivate you to
                     excel in your career.</p>
                 <div className='flex justify-center items-center z-10 relative w-[95vw]'>
@@ -358,6 +363,16 @@ const Body = () => {
                     className='absolute bottom-0 h-[50vh] w-[80vh] bg-gradient-to-r from-pink-600/30 to-purple-500/30 left-[10vh] blur-[30vh] z-0'/>
             </div>
 
+            {/*  ---------------------------------------------Wrap Up--------------------------------------------------  */}
+            <div className='container relative flex flex-col gap-8 justify-center items-center mt-10 mb-20'>
+                <div
+                    className='absolute h-[30vh] w-[60vw] top-1/2 bg-gradient-to-br from-violet-500 to-indigo-500 blur-[30vh] left-0 opacity-80 z-0'/>
+                <Image src={'/images/Decode_logo.png'} alt={"logo"} height={100} width={100} className=' z-10 mt-20 '/>
+                <h1 className={`text-6xl text-transparent bg-clip-text bg-gradient-to-b from-gray-100/80 to-gray-600/80 uppercase font-black z-10 text-center ${inter.className} `}>Then what are you<br/>waiting for?</h1>
+                <p className={`text-gray-100 font-extralight text-lg w-[50vw] text-center z-10 ${inter.className} `}>Join Decode now and start your journey to success.</p>
+                <Button className='z-10 mt-10' onClick={() => {}}>Join Now</Button>
+                <p className={`text-gray-100 font-extralight text-lg w-[50vw] text-center z-10 ${inter.className} `}>In case of any Query you can <a href='mailto:sourabhsinghredmi@gmail.com' className='text-white font-semibold cursor-pointer hover:underline'>Mail Us</a></p>
+            </div>
         </div>
     );
 };
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,31 +4,33 @@ import React from "react";
 import clsx from "clsx";
 
 interface ButtonProps {
-  type?: "button" | "submit" | "reset" | undefined;
-  children?: React.ReactNode;
-  onClick?: () => void;
-  danger?: boolean;
-  disabled?: boolean;
-  ghost?: boolean;
-  className?: string;
+    type?: "button" | "submit" | "reset" | undefined;
+    children?: React.ReactNode;
+    onClick?: () => void;
+    danger?: boolean;
+    disabled?: boolean;
+    ghost?: boolean;
+    className?: string;
+    secondary?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
-  type,
-  children,
-  onClick,
-  danger,
-  disabled,
-  ghost,
-  className,
-}) => {
-  return (
-    <button
-      onClick={onClick}
-      type={type}
-      disabled={disabled}
-      className={clsx(
-        `
+                                           type,
+                                           children,
+                                           onClick,
+                                           danger,
+                                           disabled,
+                                           ghost,
+                                           className,
+                                           secondary
+                                       }) => {
+    return (
+        <button
+            onClick={onClick}
+            type={type}
+            disabled={disabled}
+            className={clsx(
+                `
     flex 
     justify-center 
     rounded-md
@@ -40,19 +42,21 @@ const Button: React.FC<ButtonProps> = ({
     focus-visible:outline-offset-2
     text-white
     `,
-        disabled && "opacity-50 cursor-default",
-        ghost && "bg-transparent px-3 text-xl",
-        danger &&
-          "bg-rose-500 hover:bg-rose-700 focus-visible:outline-rose-900",
-        !ghost &&
-          !danger &&
-          "bg-purple-600 text-sm px-8 hover:shadow-xl hover:shadow-purple-700/40 hover:scale-110 duration-200 transition",
-        className
-      )}
-    >
-      {children}
-    </button>
-  );
+                disabled && "opacity-50 cursor-default",
+                secondary && "bg-white text-purple-500 text-xl font-semibold hover:scale-95 transition duration-400",
+                ghost && "bg-transparent px-3 text-xl hover:bg-gray-100/30 ",
+                danger &&
+                "bg-rose-500 hover:bg-rose-700 focus-visible:outline-rose-900",
+                !ghost &&
+                !danger &&
+                !secondary &&
+                "bg-purple-600 text-sm px-8 hover:shadow-xl hover:shadow-purple-700/40 hover:scale-110 duration-200 transition",
+                className
+            )}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
