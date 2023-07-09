42a635e4835b35dfca1cd2d1851635c3101d4701
void-main1812 <sour.bt21cs56@opju.ac.in>
2023-07-09 11:29:52 +0530

    ✨🔮 Completed: Auth page animation

--- app/(auth)/components/AuthSocialButton.tsx
+++ app/(auth)/components/AuthSocialButton.tsx
@@ -1,26 +1,46 @@
 import React from "react";
 import {IconType} from "react-icons";
+import useScrollAnimation from "@/hooks/useScrollAnimation";
+import {motion} from "framer-motion";
 
 interface AuthSocialButtonProps {
     icon: IconType;
     onClick: () => void;
     title: string;
+    delay?: number;
 }
 
 const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
                                                                icon: Icon,
                                                                onClick,
                                                                title,
+                                                               delay
                                                            }) => {
+
+    const {ref, mainControls} = useScrollAnimation();
+
     return (
-        <button
+        <motion.button
+            variants={{
+                hidden: {
+                    opacity: 0,
+                },
+                visible: {
+                    opacity: 1,
+                }
+            }}
+            initial='hidden'
+            animate={mainControls}
+            transition={{duration: 0.25, delay: delay}}
+            ref={ref}
             type="button"
             onClick={onClick}
-            className="inline-flex w-full -translate-y-1 hover:translate-y-0 justify-center gap-4 rounded-lg bg-gray-200 font-semibold hover:scale-95 text-gray-700 px-4 py-3 transition-all duration-75 focus:outline-offset-0"
+            className="inline-flex w-full justify-center hover:scale-110 gap-4 rounded-lg bg-gray-200 font-semibold text-gray-700 py-3 transition focus:outline-offset-0"
         >
             <Icon size={20}/>
-            <span>{title}</span>
-        </button>
+            <span>{title}
+</span>
+        </motion.button>
     );
 };
 
