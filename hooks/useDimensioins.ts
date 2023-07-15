0fb28aa2ae13a6e3b3aa9d7c1bd43c98e572a588
void-main1812 <sour.bt21cs56@opju.ac.in>
2023-07-15 11:57:20 +0530

    updated responsiveness

--- hooks/useDimensioins.ts
+++
@@ -1,8 +0,0 @@
-import React, { useRef } from "react";
-
-export const useDimensions = () => {
-  const height = useRef(window.innerHeight);
-  const width = useRef(window.innerWidth);
-
-  return { height, width };
-};
